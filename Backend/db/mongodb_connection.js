ee8f18c16e4acb49ba9f53e2742b0fee3e85e335
akilesh1706 <126157319+akilesh1706@users.noreply.github.com>
2025-01-26 20:03:31 +0530

    Update mongodb_connection.js

--- Backend/db/mongodb_connection.js
+++ Backend/db/mongodb_connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async() => {
     try {
-        await mongoose.connect('mongodb://root:example@mongo:27017/CryptoDB_Collection?authSource=admin', {
+        await mongoose.connect('mongodb://root:example@52.221.255.84:27017/CryptoDB_Collection?authSource=admin', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
